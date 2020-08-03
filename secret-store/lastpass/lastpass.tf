e7f2ab20d02088bcae8dead3879e2c2bbd712718
arau <arau@pivotal.io>
2020-08-03 12:25:05 -0400

    reorg directories

--- secret-store/lastpass/lastpass.tf
+++ secret-store/lastpass/lastpass.tf
@@ -2,13 +2,11 @@ provider "lastpass" {
   version = "~> 0.4.3"
 }
 
-variable "azure-servicePrincial-LastPassID" {
+variable "credential-LastPassID" {
   type = string
   description = "LastPass entry ID where the azure credentials are stored"
 }
 
-data "lastpass_secret" "azure-ServicePrincipal" {
-  id = var.azure-servicePrincial-LastPassID
-
-
+data "lastpass_secret" "credentials" {
+  id = var.credential-LastPassID
 }
