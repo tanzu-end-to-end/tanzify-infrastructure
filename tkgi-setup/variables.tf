aa6a8b8b9d7d0bf6c449a27e67af89e4177b105f
arau <arau@pivotal.io>
2020-08-05 01:29:02 -0400

    work on pks config

--- tkgi-setup/variables.tf
+++ tkgi-setup/variables.tf
@@ -1,6 +1,8 @@
 # ==================== Variables
 
-variable "location" {}
+variable "location" {
+
+}
 variable "subscription_id" {
   type = string
 }
@@ -8,17 +10,39 @@ variable "subscription_id" {
 variable "tenant_id" {
   type = string
 }
-variable "resource_group_name" {}
-variable "environment_name" {}
-variable "network_name" {}
-variable "ssl_certificate" {}
-variable "ssl_private_key" {}
-variable "platform_vms_security_group_name" {}
-variable "pks_as_name" {}
-variable "pks_master_managed_identity" {}
-variable "pks_worker_managed_identity" {}
-variable "pks_dns" {}
-variable "pks_lb_name" {}
+variable "resource_group_name" {
+
+}
+variable "environment_name" {
+
+}
+variable "network_name" {
+
+}
+variable "ssl_certificate" {
+
+}
+variable "ssl_private_key" {
+
+}
+variable "platform_vms_security_group_name" {
+
+}
+variable "pks_as_name" {
+
+}
+variable "pks_master_managed_identity" {
+
+}
+variable "pks_worker_managed_identity" {
+
+}
+variable "pks_dns" {
+
+}
+variable "pks_lb_name" {
+  
+}
 
 
 
