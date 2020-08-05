1515c94ff5ce744e9f1b748457b031bc65039015
arau <arau@pivotal.io>
2020-08-05 01:12:14 -0400

    work on pks config

--- tkgi-setup/create-pks-config.tf
+++ tkgi-setup/create-pks-config.tf
@@ -2,20 +2,20 @@
 data "template_file" "pks_config" {
   template = file("${path.module}/config/pks.yml")
   vars = {
-    ssl_certificate = var.ssl_certificate,
-    ssl_private_key = var.ssl_private_key,
-    platform_vms_security_group_name = var.platform_vms_security_group_name
-    location = var.location,
-    pks_as_name = var.pks_as_name,
-    resource_group_name = var.resource_group_name
-    subscription_id = var.subscription_id,
-    tenant_id = var.tenant_id,
-    network_name = var.network_name,
-    environment_name = var.environment_name,
-    pks_master_managed_identity = var.pks_master_managed_identity,
-    pks_worker_managed_identity = var.pks_worker_managed_identity
-    pks_dns = var.pks_dns,
-    pks_lb_name = var.pks_lb_name
+    ssl_certificate = "${var.ssl_certificate}",
+    ssl_private_key = "${var.ssl_private_key}",
+    platform_vms_security_group_name = "${var.platform_vms_security_group_name}",
+    location = "${var.location}",
+    pks_as_name = "${var.pks_as_name}",
+    resource_group_name = "${var.resource_group_name}",
+    subscription_id = "${var.subscription_id}",
+    tenant_id = "${var.tenant_id}",
+    network_name = "${var.network_name}",
+    environment_name = "${var.environment_name}",
+    pks_master_managed_identity = "${var.pks_master_managed_identity}",
+    pks_worker_managed_identity = "${var.pks_worker_managed_identity}",
+    pks_dns = "${var.pks_dns}",
+    pks_lb_name = "${var.pks_lb_name}"
   }
 }
 
