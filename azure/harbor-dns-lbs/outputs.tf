d8893f8f6a4a1248efe7eeb9f1b67b4441d491ae
arau <arau@pivotal.io>
2020-08-23 01:09:29 -0400

    fix output

--- azure/harbor-dns-lbs/outputs.tf
+++ azure/harbor-dns-lbs/outputs.tf
@@ -1,3 +1,3 @@
 output "harbor_domain" {
-  value = "${azurerm_dns_a_record.harbor-web}.${data.azurerm_dns_zone.hosted.name}"
+  value = "${azurerm_dns_a_record.harbor-web.name}.${data.azurerm_dns_zone.hosted.name}"
 }
--- azure/harbor-dns-lbs/provider.tf
+++ azure/harbor-dns-lbs/provider.tf
@@ -11,11 +11,3 @@ provider "azurerm" {
 terraform {
   required_version = ">= 0.12.0"
 }
-
-provider random {
-  version = "~> 2.2"
-}
-
-provider tls {
-  version = "~> 2.1"
-} 
