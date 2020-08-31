3d2f5a6ddb3e6f5a41ee00fc96231a351e65c5de
arau <arau@pivotal.io>
2020-08-31 16:00:16 -0400

    support for terraform 0.13

--- azure/harbor-dns-lbs/harbor_vm_extensions.tf
+++
@@ -1,8 +0,0 @@
-// put the values in here.
-data "template_file" "vm_extension_configuration" {
-  template = "${chomp(file("${path.module}/configuration/vm-extensions.yml"))}"
-
-  vars = {
-    environment_name  = var.environment_name
-  }
-}
--- azure/harbor-dns-lbs/outputs.tf
+++ azure/harbor-dns-lbs/outputs.tf
@@ -2,7 +2,14 @@ output "harbor_domain" {
   value = "${azurerm_dns_a_record.harbor-web.name}.${data.azurerm_dns_zone.hosted.name}"
 }
 
-output "vm-extensions-yaml" {
-  value = data.template_file.vm_extension_configuration.rendered
+output "harbor_lb_security_group_name" {
+  value = azurerm_network_security_group.harbor-network-security-group.name
 }
 
+output "harbor_lb_application_security_group_name" {
+  value = azurerm_application_security_group.harbor-app-security-group.name
+}
+
+output "harbor_web_lb_name" {
+  value = azurerm_lb.harbor-web-lb.name
+}
