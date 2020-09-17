f3fd40bd689885a9dd4c3dbcc37d9a7400095a72
arau <arau@pivotal.io>
2020-09-17 10:53:52 -0400

    fix aws compute

--- opsman/opsman-setup-scripts/opsman-setup-scripts.tf
+++ opsman/opsman-setup-scripts/opsman-setup-scripts.tf
@@ -10,9 +10,9 @@ resource "random_string" "opsman_password" {
 
 resource "null_resource" "setup_opsman" {
 
-  /*provisioner "local-exec" {
-    command = "sleep 60"
-  }*/
+  provisioner "local-exec" {
+    command = "sleep 20"
+  }
 
   provisioner "file" {
     source      = "${path.module}/scripts/"
